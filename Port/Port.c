42348edaa1209b279d72f991920d831c256ac6d7
mgtm98 <43015074+mgtm98@users.noreply.github.com>
2020-07-21 21:11:14 +0400

    Supported all the APIs, Fixed bug in the Port_ConfigType

--- Port/Port.c
+++ Port/Port.c
@@ -1,39 +1,62 @@
 #include "Port.h"
+/****************************************************************************************************************************************/
+/************************************************ GLOBAL VARIABLES **********************************************************************/
+/****************************************************************************************************************************************/
+// Container for the pins configs structs pointers. 
+const Port_ConfigType* pinsConfigs [48] = {(Port_ConfigType*)0x00};
 
-uint8_t __PortClock = 0x00;                  // used to check whether the module have a clock or not
-const Port_ConfigType* pinsConfigs [48];
+// Container to get the port GPIO struct by the port number
+GPIO_Type*       PORT_ARR [] = {GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOF};
 
-uint8_t UART_Pins [16] = {0, 1, 8, 9, 20, 21, 22, 23, 28, 29, 30, 31, 32, 33, 36, 37};
+// Container to get the pin mask by pin number
+const uint8_t    PIN_MASK [] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
 
-uint8_t __getPortNumber(const GPIO_Type *port){
-    if(port == GPIOA) return 0;
-    else if(port == GPIOB) return 1;
-    else if(port == GPIOC) return 2;
-    else if(port == GPIOD) return 3;
-    else if(port == GPIOE) return 4;
-    else if(port == GPIOF) return 5;
+// Array to detect each pin prephirals support.
+uint8_t UART_Pins   [] = {0, 1, 8, 9, 20, 21, 22, 23, 28, 29, 30, 31, 32, 33, 36, 37};
+uint8_t PWM_M0_Pins [] = {12, 13, 14, 15, 20, 21, 24, 25, 36, 37}; 
+uint8_t PWM_M1_Pins [] = {6, 7, 24, 25, 36, 37, 40, 41, 42, 43}; 
+/****************************************************************************************************************************************/
+/****************************************************************************************************************************************/
+
+
+/*
+    NOTE: field pin Port_ConfigType represents the pin number not the pin MASK
+    NOTE: to get the GPIO struct use __getPort
+    NOTE: to get the Pin mask use __getPinMask
+*/
+
+/****************************************************************************************************************************************/
+/**************************************************** HELPERS FUNCTIONS *****************************************************************/
+/****************************************************************************************************************************************/
+static __inline GPIO_Type *__getPort(Port_PinType pin){
+    return PORT_ARR[pin/8];
 }
 
+static __inline uint8_t __getPinMask(Port_PinType pin){
+    return PIN_MASK[pin%8];
+}
 
+// Initialize the port clock if it's not initialized before
 void __initPortClock(const uint8_t portNumber){
-    uint8_t mask = 1 << portNumber;
-    if (!(mask && __PortClock)){
-        __PortClock &= mask;
+    uint32_t mask = 1 << (portNumber/8);
+    if (!(mask && SYSCTL->RCGCGPIO )){
         SYSCTL->RCGCGPIO |= mask;
         while(!(SYSCTL->PRGPIO & mask));
     }
 }
 
-
+// Init rotuine for the DIO mode
 void __initPortDIOMode(const Port_ConfigType *ptr){
-    ptr->port->DATA_Bits[ptr->pin]   = ptr->options & PORT_OPTIONS_DEFAULTVAL_MASK;
-    ptr->port->DEN                   = (ptr->port->DEN & ~(ptr->pin)) | (ptr->pin & 0xFF);
-    ptr->port->DIR                   = (ptr->port->DIR & ~(ptr->pin)) | (ptr->pin & ptr->dir);
-    ptr->port->AMSEL                 = (ptr->port->AMSEL & ~(ptr->pin));
-    ptr->port->AFSEL                 = (ptr->port->AFSEL & ~(ptr->pin));
+    GPIO_Type *port             = __getPort(ptr->pin);
+    uint8_t mask                = __getPinMask(ptr->pin);
+    port->DATA_Bits[mask]       = ptr->options & PORT_OPTIONS_DEFAULTVAL_MASK;
+    port->DEN                   = port->DEN | mask;
+    port->DIR                   = (port->DIR & ~mask) | (mask & ptr->dir);
+    port->AMSEL                 = (port->AMSEL & ~mask);
+    port->AFSEL                 = (port->AFSEL & ~mask);
 }
 
-
+// Check if the pin supports the mode in the config struct
 uint8_t __checkPrephiral(uint8_t pinNumber, uint8_t *prepheralPins){
     for(uint8_t counter = 0; counter < sizeof(UART_Pins)/sizeof(UART_Pins[0]); counter++){
         if(pinNumber == prepheralPins[counter]) return 0xFF;
@@ -41,78 +64,138 @@ uint8_t __checkPrephiral(uint8_t pinNumber, uint8_t *prepheralPins){
     return 0x00;
 }
 
-
+// Init rotuine for the UART mode
 void __initPortUARTMode(const Port_ConfigType *ptr){
-    ptr->port->DEN                   = (ptr->port->DEN & ~(ptr->pin)) | (ptr->pin & 0xFF);
-    ptr->port->DIR                   = (ptr->port->DIR & ~(ptr->pin));
-    ptr->port->AMSEL                 = (ptr->port->AMSEL & ~(ptr->pin));
-    ptr->port->AFSEL                 = (ptr->port->AFSEL & ~(ptr->pin)) | (ptr->pin & 0xFF);
+    GPIO_Type *port             = __getPort(ptr->pin);
+    uint8_t mask                = __getPinMask(ptr->pin);
+    port->DEN                   = port->DEN | mask;
+    port->AFSEL                 = port->AFSEL | mask;
+    port->AMSEL                 = port->AMSEL & ~mask;
     
-    uint32_t temp = ptr->port->PCTL & ~(0x0000000F << (LOG(ptr->pin)*4));
-    ptr->port->PCTL = temp | (1 << (LOG(ptr->pin)*4));
+    uint32_t temp = port->PCTL & ~(0x0000000F << ((ptr->pin%8)*4));
+    port->PCTL = temp | (1 << (ptr->pin%8)*4);
 }
 
+// Init rotuine for the PWM mode
+void __initPortPWMMode(const Port_ConfigType *ptr, uint8_t func){
+    GPIO_Type *port             = __getPort(ptr->pin);
+    uint8_t mask                = __getPinMask(ptr->pin);
+    port->DEN                   = port->DEN | mask;
+    port->AFSEL                 = port->AFSEL | mask;
+    port->AMSEL                 = port->AMSEL & ~mask;
+    
+    uint32_t temp = port->PCTL & ~(0x0000000F << ((ptr->pin%8)*4));
+    port->PCTL = temp | (func << (ptr->pin%8)*4);
+}
+
+// Apply options to the port like Open drain and Slew rate
 void __applyOptions(const Port_ConfigType *ptr){
+    GPIO_Type *port             = __getPort(ptr->pin);
+    uint8_t mask                = __getPinMask(ptr->pin);
     // 1- pullup and pulldown resistors
     if(ptr->options && PORT_OPTIONS_PULLSTATUS_MASK == PORT_PULLUP){
-        ptr->port->PUR |= ptr->pin;
-        ptr->port->PDR &= ~(ptr->pin);
+        port->PUR |= mask;
+        port->PDR &= mask;
     }else if(ptr->options && PORT_OPTIONS_PULLSTATUS_MASK == PORT_PULLUP){
-        ptr->port->PDR |= ptr->pin;
-        ptr->port->PUR &= ~(ptr->pin);
+        port->PDR |= mask;
+        port->PUR &= ~mask;
     }else if(ptr->options && PORT_OPTIONS_PULLSTATUS_MASK == PORT_FLOAT){
-        ptr->port->PDR &= ~(ptr->pin);
-        ptr->port->PUR &= ~(ptr->pin);
+        port->PDR &= ~mask;
+        port->PUR &= ~mask;
     }
     
     // 2- Drive Current
     if(ptr->options && PORT_OPTIONS_DRCURRENT_MASK == PORT_2ma){
-        ptr->port->DR2R |= ptr->pin;
-        ptr->port->DR4R &= ~(ptr->pin);
-        ptr->port->DR8R &= ~(ptr->pin);
+        port->DR2R |=  mask;
+        port->DR4R &= ~mask;
+        port->DR8R &= ~mask;
     }else if(ptr->options && PORT_OPTIONS_PULLSTATUS_MASK == PORT_4ma){
-        ptr->port->DR2R |= ~(ptr->pin);
-        ptr->port->DR4R &= ptr->pin;
-        ptr->port->DR8R &= ~(ptr->pin);
+        port->DR2R &= ~mask;
+        port->DR4R |= mask;
+        port->DR8R &= ~mask;
     }else if(ptr->options && PORT_OPTIONS_PULLSTATUS_MASK == PORT_8ma){
-        ptr->port->DR2R &= ~(ptr->pin);
-        ptr->port->DR4R &= ~(ptr->pin);
-        ptr->port->DR8R |= ptr->pin;
+        port->DR2R &= ~mask;
+        port->DR4R &= ~mask;
+        port->DR8R |= mask;
     }else{
-        ptr->port->DR2R &= ~(ptr->pin);
-        ptr->port->DR4R &= ~(ptr->pin);
-        ptr->port->DR8R &= ~(ptr->pin);
+        port->DR2R &= ~mask;
+        port->DR4R &= ~mask;
+        port->DR8R &= ~mask;
     }
     
     // 3- Slew Rate
     if(ptr->options & PORT_OPTIONS_SLEWRATE_MASK){
-        ptr->port->SLR |= ptr->pin;
+        port->SLR |= mask;
     }else{
-        ptr->port->SLR |= ~(ptr->pin);
+        port->SLR &= ~mask;
     }
     
-    // 4- Open Drain
-    if(ptr->options & PORT_OPTIONS_OPENDRAIN_MASK){
-        ptr->port->ODR |= ptr->pin;
-    }else{
-        ptr->port->ODR |= ~(ptr->pin);
+     // 4- Open Drain
+    if(ptr->mode == PORT_DIO_MODE){
+        if(ptr->options & PORT_OPTIONS_OPENDRAIN_MASK){
+            port->ODR |= mask;
+        }else{
+            port->ODR &= ~mask;
+        }
+    }
+
+}
+
+// Initialize the pin with its mode initializer
+uint8_t __initMode(const Port_ConfigType *ConfigPtr, Port_PinModeType Mode){
+    if(Mode == PORT_DIO_MODE){
+        __initPortDIOMode(ConfigPtr);
+    }else if(Mode == PORT_UART_MODE){
+        if(!__checkPrephiral(ConfigPtr->pin, UART_Pins)) return 0;
+        __initPortUARTMode(ConfigPtr);
+    }else if(Mode == PORT_PWM_M0_MODE){
+        if(!__checkPrephiral(ConfigPtr->pin, PWM_M0_Pins)) return 0;
+        __initPortPWMMode(ConfigPtr, 4);
+    }else if(Mode == PORT_PWM_M1_MODE){
+        if(!__checkPrephiral(ConfigPtr->pin, PWM_M1_Pins)) return 0;
+        __initPortPWMMode(ConfigPtr, 5);
+    }
+    return 1;
+}
+/****************************************************************************************************************************************/
+/****************************************************************************************************************************************/
+
+
+/****************************************************************************************************************************************/
+/**************************************************** AUTOSAR APIs **********************************************************************/
+/****************************************************************************************************************************************/
+void Port_Init(const Port_ConfigType *ConfigPtr){    
+    pinsConfigs[ConfigPtr->pin] = ConfigPtr;
+    __initPortClock(ConfigPtr->pin);
+    if(!__initMode(ConfigPtr, ConfigPtr->mode))return;
+    __applyOptions(ConfigPtr);
+}
+
+void Port_SetPinDirection(Port_PinType Pin, Port_PinDirectionType Direction){
+    const Port_ConfigType *config = pinsConfigs[Pin];
+    if(config != (Port_ConfigType*)0x00 && config->options & PORT_OPTIONS_DIRCHANGE_MASK){
+        uint8_t mask = __getPinMask(config->pin);
+        GPIO_Type *port = __getPort(config->pin);
+        port->DIR = (port->DIR & ~mask) | (mask & Direction);
+    }        
+}
+
+void Port_RefreshPortDirection(void){
+    for(int a = 0; a < 48; a++){
+        const Port_ConfigType *config = pinsConfigs[a];
+        if(config != (Port_ConfigType*)0x00 && !(config->options & PORT_OPTIONS_DIRCHANGE_MASK)){
+            uint8_t mask = __getPinMask(config->pin);
+            GPIO_Type *port = __getPort(config->pin);
+            port->DIR = (port->DIR & ~mask) | (mask & config->dir);
+        }
     }
 }
 
-
-void Port_Init(const Port_ConfigType *ConfigPtr){
-    uint8_t portNumber = __getPortNumber(ConfigPtr->port);
-    uint8_t pinNumber = portNumber*8 + LOG(ConfigPtr->pin);
-    pinsConfigs[pinNumber] = ConfigPtr;
-    __initPortClock(portNumber);
-    if(ConfigPtr->mode == PORT_DIOMODE){
-        __initPortDIOMode(ConfigPtr);
-    }else if(ConfigPtr->mode == PORT_UARTMODE){
-        if(!__checkPrephiral(pinNumber, UART_Pins)) return;
-        __initPortUARTMode(ConfigPtr);
-    }else if(ConfigPtr->mode == PORT_PWMMODE){
-//        __checkPrephiral(pinNumber, UART_Pins);
-//        __initPortUARTMode(ConfigPtr);
-    }
-    __applyOptions(ConfigPtr);
-}    
+void Port_SetPinMode(Port_PinType Pin, Port_PinModeType Mode){
+    const Port_ConfigType *config = pinsConfigs[Pin];
+    if(config != (Port_ConfigType*)0x00 && config->options & PORT_OPTIONS_MODCHANGE_MASK){
+        __initMode(config, Mode);
+    }  
+}
+/****************************************************************************************************************************************/
+/****************************************************************************************************************************************/
--- Port/Port.h
+++ Port/Port.h
@@ -1,6 +1,10 @@
 #include "include/tm4c123gh6pm.h"
 #include "MCAL/sysctrl.h"
-    
+
+// Autosar Port Module:
+// - used for initialize the pins for various modes
+// - now supports initializers for DIO, PWM, UART
+
 #define    LOG(X)                      (31 - __clz(X))
 
 #define    STD_ON                       0x00
@@ -14,6 +18,55 @@
 #define    PORT_OPTIONS_SLEWRATE_MASK    0b0100000000000000
 #define    PORT_OPTIONS_OPENDRAIN_MASK   0b1000000000000000
 
+#define    PORT_A_PIN_0                 0
+#define    PORT_A_PIN_1                 1
+#define    PORT_A_PIN_2                 2
+#define    PORT_A_PIN_3                 3
+#define    PORT_A_PIN_4                 4
+#define    PORT_A_PIN_5                 5
+#define    PORT_A_PIN_6                 6
+#define    PORT_A_PIN_7                 7
+
+#define    PORT_B_PIN_0                 8
+#define    PORT_B_PIN_1                 9
+#define    PORT_B_PIN_2                 10
+#define    PORT_B_PIN_3                 11
+#define    PORT_B_PIN_4                 12
+#define    PORT_B_PIN_5                 13
+#define    PORT_B_PIN_6                 14
+#define    PORT_B_PIN_7                 15
+
+#define    PORT_C_PIN_0                 16
+#define    PORT_C_PIN_1                 17
+#define    PORT_C_PIN_2                 18
+#define    PORT_C_PIN_3                 19
+#define    PORT_C_PIN_4                 20
+#define    PORT_C_PIN_5                 21
+#define    PORT_C_PIN_6                 22
+#define    PORT_C_PIN_7                 23
+
+#define    PORT_D_PIN_0                 24
+#define    PORT_D_PIN_1                 25
+#define    PORT_D_PIN_2                 26
+#define    PORT_D_PIN_3                 27
+#define    PORT_D_PIN_4                 28
+#define    PORT_D_PIN_5                 29
+#define    PORT_D_PIN_6                 30
+#define    PORT_D_PIN_7                 31
+
+#define    PORT_E_PIN_0                 32
+#define    PORT_E_PIN_1                 33
+#define    PORT_E_PIN_2                 34
+#define    PORT_E_PIN_3                 35
+#define    PORT_E_PIN_4                 36
+#define    PORT_E_PIN_5                 37
+
+#define    PORT_F_PIN_0                 40
+#define    PORT_F_PIN_1                 41
+#define    PORT_F_PIN_2                 42
+#define    PORT_F_PIN_3                 43
+#define    PORT_F_PIN_4                 44
+
 #define PORT_PIN0                       0x01
 #define PORT_PIN1                       0x02
 #define PORT_PIN2                       0x04
@@ -24,9 +77,10 @@
 #define PORT_PIN7                       0x80
 #define PORT_ALLPINS                    0xFF
 
-#define PORT_DIOMODE                    0x00
-#define PORT_UARTMODE                   0x01
-#define PORT_PWMMODE                    0x02
+#define PORT_DIO_MODE                   0x00
+#define PORT_UART_MODE                  0x01
+#define PORT_PWM_M0_MODE                0x02
+#define PORT_PWM_M1_MODE                0x03
 
 
 typedef enum {
@@ -48,30 +102,29 @@ typedef enum {
     PORT_8ma        = 0b0001000000000000,
 } Port_DriveCurrentType;
 
-// Defines whether the pin is ADC, DIO, SPI
+// Defines pin number
 typedef uint8_t Port_PinType;
 
-// Pin MASK to  access a specific pin in the port
+// Defines whether the pin is ADC, DIO, SPI
 typedef uint8_t Port_PinModeType;
 
 // Options field consisits of 16 bit fields each on responsible for a specific option in the Pin
 //                -----------------------------------------
 //                | 15 | 14 | 13-12 | 11-10 | 9 | 8 | 7-0 |
 //                -----------------------------------------
-// BIT 0-7:   Default value of the pin    (0x00 means LOW, 0xFF means HIGH)
-// BIT 8:   Direction Changeable          (0 means Not Changeable, 1 means Changeable)
-// BIT 9:   Mode Changeable               (0 means Not Changeable, 1 means Changeable)
+// BIT 0-7:   Default value of the pin     (0x00 means LOW, 0xFF means HIGH)
+// BIT 8:     Direction Changeable         (0 means Not Changeable, 1 means Changeable)
+// BIT 9:     Mode Changeable              (0 means Not Changeable, 1 means Changeable)
 // BIT 10-11: Specifies the pull status   
 // BIT 12-13: Specifies the drive current 
-// BIT 14:   Slew Rate                    (0 means not activated, 1 means activated)
-// BIT 15:   Open drain                   (0 means not activated, 1 means activated)
+// BIT 14:    Slew Rate                    (0 means not activated, 1 means activated)
+// BIT 15:    Open drain                   (0 means not activated, 1 means activated)
 //
 // NOTE: Slew Rate can't be activated unless the drive current is PORT_8ma
 typedef uint16_t Port_OptionsType;
 
 
 typedef struct{
-    GPIO_Type              *port;           // Pointer to GPIO struct in memory CMSIS style
     Port_OptionsType        options;        // 16bit field containes options various options
     Port_PinType            pin;            // 8bit field represents the pin number (can represent more than one pin)
     Port_PinModeType        mode;           // defines the pin to be either DIO, ADC, PWM, etc.
